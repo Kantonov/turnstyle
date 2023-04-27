4e3445d4df341be13b1f27f53e4195eb6fcf871b
Konstantin Antonov <konstantin.m.antonov@gmail.com>
2023-04-27 13:25:37 +0300

    fix progress runs fetch logic

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,25 @@ import { Waiter } from "./wait";
 
 async function run() {
   try {
+    /**
+     * Local debug setup:
+      const input = {
+        // implicit inputs
+        githubToken: "token", // careful not to git push this
+        owner: "owner-string",
+        repo: "test-repo",
+        workflowName: "Case Sensitive Name", // taken from the "name: ..." on top of the action.yml
+        branch: "master",
+        runId: 111111111111, // big number to have the action wait, meaning this one is the latest
+        
+        // action inputs
+        pollIntervalSeconds: 5,
+        continueAfterSeconds: undefined,
+        abortAfterSeconds: undefined,
+        sameBranchOnly: true,
+        cancelIfNotLatest: false
+      };
+     */
     const input = parseInput(env);
     const github = new OctokitGitHub(input.githubToken);
     const workflows = await github.workflows(input.owner, input.repo);
